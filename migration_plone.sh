df85723dd7f3d37679475a8e677d1ba36537bf17
Daniel Plominski <daniel@notebook1-plitc.global.plitc.eu>
2015-10-31 01:15:46 +0100

    Version: 0.1.5.8

--- migration_plone.sh
+++ migration_plone.sh
@@ -373,7 +373,58 @@ if [ "$TARGETPLONEVERSION" = "4" ]
 then
    #/ install plone 4
    showyellow "install plone for: $TARGETJAIL"
-   jexec "$(tjailid)" pkg install -y plone wv xpdf freetype2 ltxml
+   jexec "$(tjailid)" pkg install -y plone wv xpdf freetype2 ltxml graphviz
+   (sleep 4) & spinner $!
+
+   #/ fix: PATH
+   cat << "CSHRC" > /tmp/migration_plone_cshrc
+# $FreeBSD: releng/9.3/etc/root/dot.cshrc 244005 2012-12-08 00:25:51Z eadler $
+#
+# .cshrc - csh resource script, read at beginning of execution by each shell
+#
+# see also csh(1), environ(7).
+# more examples available at /usr/share/examples/csh/
+#
+
+alias h         history 25
+alias j         jobs -l
+alias la        ls -aF
+alias lf        ls -FA
+alias ll        ls -lAF
+
+# A righteous umask
+umask 22
+
+set path = (/sbin /bin /usr/sbin /usr/bin /usr/games /usr/local/sbin /usr/local/bin $HOME/bin /usr/local/libexec/xpdf)
+
+setenv  EDITOR  vi
+setenv  PAGER   more
+setenv  BLOCKSIZE       K
+
+if ($?prompt) then
+        # An interactive shell -- set some stuff up
+        set prompt = "%N@%m:%~ %# "
+        set promptchars = "%#"
+
+        set filec
+        set history = 1000
+        set savehist = (1000 merge)
+        set autolist = ambiguous
+        # Use history to aid expansion
+        set autoexpand
+        set autorehash
+        set mail = (/var/mail/$USER)
+        if ( $?tcsh ) then
+                bindkey "^W" backward-delete-word
+                bindkey -k up history-search-backward
+                bindkey -k down history-search-forward
+        endif
+
+endif
+CSHRC
+   cp -f /tmp/migration_plone_cshrc "$(newjailpath)"/root/.cshrc
+   jexec "$(tjailid)" ln -s /usr/local/bin/wvHtml /usr/local/bin/wvhtml
+   jexec "$(tjailid)" hash -r
    (sleep 4) & spinner $!
 
    #/ fix: libiconv.so
